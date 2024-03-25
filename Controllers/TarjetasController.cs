7078da5df418a0500f1c65d56a97b7fbfb34b03d
DGO2101 <pit_olimpo@yahoo.com>
2024-03-25 03:35:53 +0100

    todo funciona

--- Controllers/TarjetasController.cs
+++ Controllers/TarjetasController.cs
@@ -1,65 +1,68 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.Rendering;
-    using Microsoft.EntityFrameworkCore;
-    using WebAppBanco.Data;
-    using WebAppBanco.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml; // Librería para trabajar con Excel
+using WebAppBanco.Data;
+using WebAppBanco.Models;
 
-    namespace WebAppBanco4.Controllers
+namespace WebAppBanco4.Controllers
+{
+    public class TarjetasController : Controller
     {
-        public class TarjetasController : Controller
+        private readonly WebAppBancoContext _context;
+
+        public TarjetasController(WebAppBancoContext context)
         {
-            private readonly WebAppBancoContext _context;
+            _context = context;
+        }
 
-            public TarjetasController(WebAppBancoContext context)
+        // GET: Tarjetas
+        // Muestra la lista de todas las tarjetas de crédito
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tarjetas.ToListAsync());
+        }
+
+        // GET: Tarjetas/Details/5
+        // Muestra los detalles de una tarjeta específica
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
             {
-                _context = context;
+                return NotFound();
             }
 
-            // GET: Tarjetas
-            public async Task<IActionResult> Index()
+            var tarjetas = await _context.Tarjetas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tarjetas == null)
             {
-                return View(await _context.Tarjetas.ToListAsync());
+                return NotFound();
             }
 
-            // GET: Tarjetas/Details/5
-            public async Task<IActionResult> Details(int? id)
-            {
-                if (id == null)
-                {
-                    return NotFound();
-                }
+            return View(tarjetas);
+        }
 
-                var tarjetas = await _context.Tarjetas
-                    .FirstOrDefaultAsync(m => m.Id == id);
-                if (tarjetas == null)
-                {
-                    return NotFound();
-                }
+        //GET: Tarjetas/Create
+        // Muestra el formulario para crear una nueva tarjeta de crédito
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-                return View(tarjetas);
-            }
-
-            //GET: Tarjetas/Create
-            public IActionResult Create()
-            {
-                return View();
-            }
-
-            // POST: Tarjetas/Create
-            // To protect from overposting attacks, enable the specific properties you want to bind to.
-            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Create([Bind("Id,Nombres,Apellidos,Numero")] Tarjetas tarjetas)
-            {
-                    _context.Add(tarjetas);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
-            }
+        // POST: Tarjetas/Create
+        // Crea una nueva tarjeta de crédito en la base de datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombres,Apellidos,Numero")] Tarjetas tarjetas)
+        {
+            _context.Add(tarjetas);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
         // GET: Tarjetas/AgregarCompra/5
         public IActionResult AgregarCompra(int id)
@@ -78,6 +81,25 @@
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             
+        }
+
+        // GET: Tarjetas/AgregarPago/5
+        public IActionResult AgregarPago(int id)
+        {
+            ViewBag.TarjetaId = id;
+            return View();
+        }
+
+        // POST: Tarjetas/AgregarPago/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AgregarPago(int id, [Bind("Fecha,Descripcion,Monto")] Pagos pago)
+        {
+            pago.TarjetaId = id;
+            _context.Add(pago);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
 
         // GET: Tarjetas/VerCompras/5
@@ -110,13 +132,27 @@
 
             return View(model);
         }
+        public async Task<IActionResult> Transacciones(int? id)
+        {
+            // Obtener el primer día del mes actual
+            var primerDiaMesActual = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
 
+            // Obtener las compras y pagos realizados en el mes actual para la tarjeta especificada, ordenados por fecha
+            var compras = await _context.Compras
+                .Where(c => c.TarjetaId == id && c.Fecha >= primerDiaMesActual)
+                .OrderBy(c => c.Fecha)
+                .ToListAsync();
 
+            var pagos = await _context.Pagos
+                .Where(p => p.TarjetaId == id && p.Fecha >= primerDiaMesActual)
+                .OrderBy(p => p.Fecha)
+                .ToListAsync();
 
+            // Crear la tupla con las compras y pagos del mes actual
+            var model = new Tuple<IEnumerable<Compras>, IEnumerable<Pagos>>(compras, pagos);
 
-
-
-
+            return View(model);
+        }
 
 
         // GET: Tarjetas/Edit/5
@@ -134,7 +170,7 @@
                 }
                 return View(tarjetas);
             }
-
+        //edita los objetos Tarjeta creados
             // POST: Tarjetas/Edit/5
             // To protect from overposting attacks, enable the specific properties you want to bind to.
             // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -165,6 +201,7 @@
                     return RedirectToAction(nameof(Index));
             }
 
+        //borra los objetos Tarjeta creados
             // GET: Tarjetas/Delete/5
             public async Task<IActionResult> Delete(int? id)
             {
@@ -198,7 +235,28 @@
                 return RedirectToAction(nameof(Index));
             }
 
-            private bool TarjetasExists(int id)
+        //exporta a Excel la lista de compras
+        public ActionResult ExportarExcel()
+        {
+            var compras = _context.Compras.ToList();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Compras");
+
+                worksheet.Cells.LoadFromCollection(compras, true);
+
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Compras.xlsx");
+            }
+        }
+
+        private bool TarjetasExists(int id)
             {
                 return _context.Tarjetas.Any(e => e.Id == id);
             }
